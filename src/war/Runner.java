655407f2797f18c492a65b28de6bd0a99af8fcb2
super <super@172.24.51.37>
2017-09-30 12:40:16 -0500

    1sr commit just to see if I did this right

---
+++ src/war/Runner.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package war;
+
+/**
+ * @author super
+ *
+ */
+public class Runner {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
